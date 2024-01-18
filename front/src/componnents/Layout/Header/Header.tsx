1e482d7c0d2bae209ab8d883d0b0b932e5615c0a
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2024-01-18 11:26:56 +0100

    Get home link in header

--- front/src/componnents/Layout/Header/Header.tsx
+++ front/src/componnents/Layout/Header/Header.tsx
@@ -1,26 +1,31 @@
 import "./Header.scss";
-import {
-  IconDefinition,
-  IconLookup,
-  findIconDefinition,
-} from "@fortawesome/fontawesome-svg-core";
 import BasketIcon from "../../Shared/icons/basketIcon";
 import BurgerMenu from "../../BurgerMenu/BurgerMenu";
+import { useState } from "react";
+import Modal from "../../Shared/modals/modal";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  const basketLookup: IconLookup = {
-    prefix: "fas",
-    iconName: "basket-shopping",
-  };
-  const basketIconDefinition: IconDefinition = findIconDefinition(basketLookup);
+  const [isBasketModalOpen, setIsBasketModalOpen] = useState<boolean>(false);
+
   return (
-    <header>
-      <BurgerMenu />
-      <h1 className="brand-name">La Marmotte Habile</h1>
-      <button>
-        <BasketIcon />
-      </button>
-    </header>
+    <>
+      <header>
+        <BurgerMenu />
+        <Link to={"/"}>
+          <h1 className="brand-name">La Marmotte Habile</h1>
+        </Link>
+        <button className="button-icon">
+          <BasketIcon />
+        </button>
+      </header>
+      <Modal isOpen={isBasketModalOpen}>
+        <p>
+          Cette fonctionnalité est en cours de développement. Pas de panique,
+          elle sera bientôt prête !
+        </p>
+      </Modal>
+    </>
   );
 };
 
