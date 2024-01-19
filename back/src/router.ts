6333ce1c56b0e595dbd530ace5927b56f586e665
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2024-01-19 11:04:38 +0100

    back - create get route for test

--- back/src/router.ts
+++ back/src/router.ts
@@ -1,5 +1,9 @@
 import express from 'express';
-
+import { Request, Response } from 'express';
 const router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
+    return res.status(200).json({ message: 'Ok' });
+});
 
 export default router;
