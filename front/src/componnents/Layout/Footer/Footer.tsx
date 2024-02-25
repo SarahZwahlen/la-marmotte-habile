b85b0fbfc955cf4ed01996aca741af7964d3001a
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2024-02-25 15:35:31 +0100

    front - footer comment github link

--- front/src/componnents/Layout/Footer/Footer.tsx
+++ front/src/componnents/Layout/Footer/Footer.tsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <footer>
       <h2>La Marmotte Habile</h2>
-      <a href="https://github.com/SarahZwahlen">
+      {/* <a href="https://github.com/SarahZwahlen">
         Site réalisé par Sarah Zwahlen
         <img src={octocat} />
-      </a>
+      </a> */}
       <p>Tous droits réservés - 2023</p>
     </footer>
   );
